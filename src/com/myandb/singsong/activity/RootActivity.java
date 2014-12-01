6517cc46fe66eea24735e953dfa3a99db71007df
mamondbaltob <mun@myandb.com>
2014-12-01 19:11:55 +0900

    액션바 디자인

--- src/com/myandb/singsong/activity/RootActivity.java
+++ src/com/myandb/singsong/activity/RootActivity.java
@@ -8,20 +8,29 @@ import com.myandb.singsong.dialog.LoginDialog;
 import com.myandb.singsong.fragment.DrawerFragment;
 import com.myandb.singsong.fragment.HomeFragment;
 import com.myandb.singsong.service.PlayerService;
+import com.myandb.singsong.util.Logger;
 import com.myandb.singsong.widget.SlidingPlayerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
+@SuppressLint("InlinedApi")
 public class RootActivity extends BaseActivity {
 	
 	public static final String SAVED_STATE_ACTION_BAR_HIDDEN = "saved_state_action_bar_hidden";
@@ -30,6 +39,7 @@ public class RootActivity extends BaseActivity {
 	private SlidingMenu drawer;
 	private SlidingPlayerLayout slidingPlayerLayout;
 	private LoginDialog loginDialog;
+	private TextView tvNotificationCount;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -73,9 +83,22 @@ public class RootActivity extends BaseActivity {
 		ActionBar actionBar = getSupportActionBar();
 		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
+		setHomeButtonRightMargin(0);
 		
 		FragmentManager manager = getSupportFragmentManager();
 		manager.addOnBackStackChangedListener(onBackStackChangedListener);
+	}
+	
+	private void setHomeButtonRightMargin(int pixel) {
+		View home = findViewById(android.R.id.home);
+		if (home != null) {
+			android.view.ViewGroup.LayoutParams layoutParams = home.getLayoutParams();
+			if (layoutParams instanceof FrameLayout.LayoutParams) {
+				FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) layoutParams;
+				lp.rightMargin = pixel;
+				home.setLayoutParams(lp);
+			}
+		}
 	}
 	
 	private OnBackStackChangedListener onBackStackChangedListener = new OnBackStackChangedListener() {
@@ -85,9 +108,9 @@ public class RootActivity extends BaseActivity {
 			ActionBar actionBar = getSupportActionBar();
 			FragmentManager manager = getSupportFragmentManager();
 			if (manager.getBackStackEntryCount() > 1) {
-				actionBar.setHomeAsUpIndicator(null);
+				actionBar.setHomeAsUpIndicator(R.drawable.ic_action_back);
 			} else {
-				actionBar.setHomeAsUpIndicator(null);
+				actionBar.setHomeAsUpIndicator(R.drawable.ic_action_drawer);
 			}
 		}
 	};
@@ -171,8 +194,72 @@ public class RootActivity extends BaseActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.home, menu);
+		
+		initializeNotificationCountView(menu);
+		updateNotificationCount();
+		registerNotificationCountListener();
+		
 		return super.onCreateOptionsMenu(menu);
 	}
+	
+	private void initializeNotificationCountView(final Menu menu) {
+		if (tvNotificationCount == null) {
+			View notificationView = getActionViewCompat(menu, R.id.action_notification);
+			if (notificationView != null) {
+				notificationView.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						Logger.log("click");
+					}
+				});
+				tvNotificationCount = (TextView) notificationView.findViewById(R.id.tv_action_notification_count);
+			}
+		}
+	}
+	
+	private View getActionViewCompat(Menu menu, int id) {
+		return MenuItemCompat.getActionView(menu.findItem(id));
+	}
+	
+	private void updateNotificationCount() {
+		int count = getCurrentNotificationCount();
+		setNotificationNum(count, tvNotificationCount);
+	}
+	
+	private int getCurrentNotificationCount() {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		String key = getString(R.string.key_notification_count);
+		return preferences.getInt(key, 0);
+	}
+	
+	private void setNotificationNum(int count, TextView textView) {
+		if (textView != null) {
+			if (count > 0) {
+				count = Math.min(count, 99);
+				textView.setVisibility(View.VISIBLE);
+				textView.setText(String.valueOf(count));
+			} else {
+				textView.setVisibility(View.GONE);
+			}
+		}
+	}
+	
+	private void registerNotificationCountListener() {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		preferences.registerOnSharedPreferenceChangeListener(notificationCountChangeListener);
+	}
+	
+	private OnSharedPreferenceChangeListener notificationCountChangeListener = new OnSharedPreferenceChangeListener() {
+		
+		@Override
+		public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+			String countkey = getString(R.string.key_notification_count);
+			if (key.equals(countkey)) {
+				updateNotificationCount();
+			}
+		}
+	};
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -184,6 +271,10 @@ public class RootActivity extends BaseActivity {
 			} else {
 				drawer.toggle();
 			}
+			return true;
+			
+		case R.id.action_sing:
+			Logger.log("sing");
 			return true;
 		
 		default:
