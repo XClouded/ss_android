1788cb59323ab0be576981788569e69ae07ed6b2
mamondbaltob <mun@myandb.com>
2014-12-12 19:36:30 +0900

    친구초대 메뉴로 빼기

--- src/com/myandb/singsong/fragment/FindFriendsFragment.java
+++ src/com/myandb/singsong/fragment/FindFriendsFragment.java
@@ -11,6 +11,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -18,7 +21,6 @@ import android.widget.Button;
 public class FindFriendsFragment extends ListFragment {
 	
 	private Button btnSearchUser;
-	private Button btnInviteFriends;
 	private Button btnConnectFacebook;
 	private View vFacebookHeaderContainer;
 
@@ -38,7 +40,6 @@ public class FindFriendsFragment extends ListFragment {
 	protected void onViewInflated(View view, LayoutInflater inflater) {
 		super.onViewInflated(view, inflater);
 		btnSearchUser = (Button) view.findViewById(R.id.btn_search_user);
-		btnInviteFriends = (Button) view.findViewById(R.id.btn_invite_friends);
 		btnConnectFacebook = (Button) view.findViewById(R.id.btn_connect_facebook);
 		vFacebookHeaderContainer = view.findViewById(R.id.v_facebook_header_container);
 	}
@@ -47,7 +48,6 @@ public class FindFriendsFragment extends ListFragment {
 	protected void setupViews() {
 		super.setupViews();
 		btnSearchUser.setOnClickListener(searchUserClickListener);
-		btnInviteFriends.setOnClickListener(inviteFriendsClickListener);
 		btnConnectFacebook.setOnClickListener(connectFacebookClickListener);
 	}
 	
@@ -59,10 +59,23 @@ public class FindFriendsFragment extends ListFragment {
 		}
 	};
 	
-	private OnClickListener inviteFriendsClickListener = new OnClickListener() {
+	private OnClickListener connectFacebookClickListener = new OnClickListener() {
 		
 		@Override
 		public void onClick(View v) {
+			
+		}
+	};
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.find_friends, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_invite_friends:
 			Bundle bundle = new Bundle();
 			bundle.putInt(FragmentPagerFragment.EXTRA_ITEM_NUM, 0);
 			bundle.putString(FragmentPagerFragment.EXTRA_PAGER_ADAPTER, InviteFriendsPagerAdapter.class.getName());
@@ -71,16 +84,12 @@ public class FindFriendsFragment extends ListFragment {
 			intent.putExtra(BaseActivity.EXTRA_FRAGMENT_NAME, FragmentPagerFragment.class.getName());
 			intent.putExtra(BaseActivity.EXTRA_FRAGMENT_BUNDLE, bundle);
 			startFragment(intent);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-	};
-	
-	private OnClickListener connectFacebookClickListener = new OnClickListener() {
-		
-		@Override
-		public void onClick(View v) {
-			
-		}
-	};
+	}
 
 	@Override
 	protected void onDataChanged() {
