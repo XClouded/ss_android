de4fee730d66771cf67947b01dc0f7b2693e7ece
mamondbaltob <mun@myandb.com>
2014-12-18 18:30:00 +0900

    facebook taggable 관련 파일 삭제

--- src/com/myandb/singsong/fragment/FacebookFriendsFragment.java
+++ src/com/myandb/singsong/fragment/FacebookFriendsFragment.java
@@ -2,9 +2,4 @@ package com.myandb.singsong.fragment;
 
 public class FacebookFriendsFragment extends ListFragment {
 
-	@Override
-	protected void onDataChanged() {
-		super.onDataChanged();
-	}
-
 }
