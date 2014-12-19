98378a2c4bcedadf19a22eafb979cdb4dc4771a6
mamondbaltob <mun@myandb.com>
2014-12-19 14:11:52 +0900

    페북 친구 초대 구현 (노 디자인, 노 링크)

--- src/com/myandb/singsong/fragment/InviteFacebookFragment.java
+++ src/com/myandb/singsong/fragment/InviteFacebookFragment.java
@@ -1,5 +1,70 @@
 package com.myandb.singsong.fragment;
 
-public class InviteFacebookFragment extends ListFragment {
+import com.facebook.widget.FacebookDialog.MessageDialogBuilder;
+import com.myandb.singsong.GoogleStore;
+import com.myandb.singsong.R;
+import com.myandb.singsong.Store;
+import com.myandb.singsong.net.UrlBuilder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class InviteFacebookFragment extends BaseFragment {
+	
+	private Button btnInviteFacebook;
+
+	@Override
+	protected int getResourceId() {
+		return R.layout.fragment_invite_facebook;
+	}
+
+	@Override
+	protected void onViewInflated(View view, LayoutInflater inflater) {
+		btnInviteFacebook = (Button) view.findViewById(R.id.btn_invite_facebook);
+	}
+
+	@Override
+	protected void initialize(Activity activity) {}
+
+	@Override
+	protected void setupViews(Bundle savedInstanceState) {
+		btnInviteFacebook.setOnClickListener(inviteFacebookClickListener);
+	}
+	
+	private OnClickListener inviteFacebookClickListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			MessageDialogBuilder builder = new MessageDialogBuilder(getActivity());
+			if (builder.canPresent()) {
+				showFacebookMessageDialog(builder);
+			} else {
+				moveToFacebookMessengerOnGoogleStore();
+			}
+		}
+	};
+	
+	private void showFacebookMessageDialog(MessageDialogBuilder builder) {
+		builder.setLink(new UrlBuilder().toString())
+			.setName(getString(R.string.app_name))
+			.setCaption(getString(R.string.app_name))
+			.setPicture(new UrlBuilder().s("img").s("actionbar_logo.png").toString())
+			.setDescription("노래도 부르고 짝도 찾는 콜라보 노래방과 함께 해보세요.")
+			.setFragment(this)
+			.build()
+			.present();
+	}
+	
+	private void moveToFacebookMessengerOnGoogleStore() {
+		Store store = new GoogleStore("com.facebook.orca");
+		store.move(getActivity());
+	}
+
+	@Override
+	protected void onDataChanged() {}
 
 }
