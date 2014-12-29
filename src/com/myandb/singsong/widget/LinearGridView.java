b8df2f5fb1884c06b6190952c56a332c98977904
mamondbaltob <mun@myandb.com>
2014-12-29 15:19:59 +0900

    친구찾기 디자인 listview -> lineargridview 로 변경

--- src/com/myandb/singsong/widget/LinearGridView.java
+++ src/com/myandb/singsong/widget/LinearGridView.java
@@ -6,14 +6,16 @@ import android.content.Context;
 import android.database.DataSetObserver;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 
 public class LinearGridView extends LinearLayout {
 	
-	private int columnCount = 1;
 	private ListAdapter adapter;
 	private DataSetObserver observer;
+	private int columnCount = 1;
+	private boolean showDivider = false;
 
 	public LinearGridView(Context context) {
 		super(context);
@@ -35,6 +37,10 @@ public class LinearGridView extends LinearLayout {
 	
 	public ListAdapter getAdapter() {
 		return adapter;
+	}
+	
+	public void setShowDivider(boolean show) {
+		showDivider = show;
 	}
 	
 	public void setAdapter(ListAdapter adapter) {
@@ -64,14 +70,21 @@ public class LinearGridView extends LinearLayout {
 			if (isNewRowStart(i)) {
 				if (row != null) {
 					addViewInLayout(row, -1, row.getLayoutParams(), true);
+					if (showDivider && !isLastItem(i, l)) {
+						View divider = getDivider();
+						addViewInLayout(divider, -1, divider.getLayoutParams(), true);
+					}
 				}
 				row = makeRow();
 			}
 			
 			View child = getChild(i);
 			row.addView(child);
+			
 		}
-		addViewInLayout(row, -1, row.getLayoutParams(), true);
+		if (row != null) {
+			addViewInLayout(row, -1, row.getLayoutParams(), true);
+		}
 	}
 	
 	private boolean isNewRowStart(int position) {
@@ -86,37 +99,23 @@ public class LinearGridView extends LinearLayout {
 		return row;
 	}
 	
-	private View getChild(int position) {
+	protected View getChild(int position) {
 		View child = adapter.getView(position, null, this);
 		LayoutParams params = new LayoutParams(0, LayoutParams.WRAP_CONTENT, 1);
 		child.setLayoutParams(params);
-		setChildBackground(child, position);
 		return child;
 	}
 	
-	private void setChildBackground(View child, int position) {
-		switch (position) {
-		case 0:
-		case 2:
-			child.setBackgroundResource(R.drawable.drawer_right_bottom_selector);
-			break;
-			
-		case 1:
-		case 3:
-			child.setBackgroundResource(R.drawable.drawer_bottom_selector);
-			break;
-			
-		case 4:
-			child.setBackgroundResource(R.drawable.drawer_right_selector);
-			break;
-			
-		case 5:
-			child.setBackgroundResource(R.drawable.drawer_selector);
-			break;
-
-		default:
-			break;
-		}
+	private View getDivider() {
+		ImageView divider = new ImageView(getContext());
+		LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT, 1);
+		divider.setImageResource(R.color.divider);
+		divider.setLayoutParams(params);
+		return divider;
+	}
+	
+	private boolean isLastItem(int position, int length) {
+		return position == length - 1;
 	}
 	
 	private class AdapterDataSetObserver extends DataSetObserver {
