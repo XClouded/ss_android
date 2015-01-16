ad01c1b1f6ae1f20235b65fc01d79b1d8bd94def
mamondbaltob <mun@myandb.com>
2015-01-16 12:17:44 +0900

    viewpager height wrap_content 가능하게

--- src/com/myandb/singsong/widget/InfiniteViewPager.java
+++ src/com/myandb/singsong/widget/InfiniteViewPager.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.View;
 
 public class InfiniteViewPager extends ViewPager {
 
@@ -20,7 +21,6 @@ public class InfiniteViewPager extends ViewPager {
     @Override
     public void setAdapter(PagerAdapter adapter) {
         super.setAdapter(adapter);
-        
         setCurrentItem(0);
     }
 
@@ -48,5 +48,22 @@ public class InfiniteViewPager extends ViewPager {
             return 0;
         }
     }
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
+		int height = 0;
+		for (int i = 0, l = getChildCount(); i < l; i++) {
+			View child = getChildAt(i);
+			child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+			int h = child.getMeasuredHeight();
+			height = Math.max(h, height);
+		}
+		
+		heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+		
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
 
 }
