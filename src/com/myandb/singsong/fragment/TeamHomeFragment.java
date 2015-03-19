f81d23ec0863def072885ba134c3892d3381a450
mamondbaltob <mun@myandb.com>
2015-03-19 13:59:41 +0900

    floating 구현해봄

---
+++ src/com/myandb/singsong/fragment/TeamHomeFragment.java
@@ -0,0 +1,32 @@
+package com.myandb.singsong.fragment;
+
+import com.myandb.singsong.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class TeamHomeFragment extends BaseFragment {
+
+	@Override
+	protected int getResourceId() {
+		return R.layout.fragment_team_home;
+	}
+
+	@Override
+	protected void onViewInflated(View view, LayoutInflater inflater) {
+		
+	}
+
+	@Override
+	protected void initialize(Activity activity) {}
+
+	@Override
+	protected void setupViews(Bundle savedInstanceState) {
+	}
+
+	@Override
+	protected void onDataChanged() {}
+
+}
